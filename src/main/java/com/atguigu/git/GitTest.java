d3366451b8e6c60b3da0dde070985df4f17f899b
Furse <450067501@qq.com>
2023-10-30 15:03:14 +0800

    hot-fix first commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("hello Git2");
-
+        System.out.println("hot-fix1");
     }
 }
