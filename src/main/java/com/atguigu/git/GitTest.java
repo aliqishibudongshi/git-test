689c882fb76428c880b53768cba53e9fbbe5ae24
aliqishibudongshi <46096033+aliqishibudongshi@users.noreply.github.com>
2023-10-30 16:20:10 +0800

    pull test first commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello Git2");
         System.out.println("hot-fix1");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
